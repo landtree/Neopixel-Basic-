b7b4b3f580ad76b17343c15e26ae359d1d1e9ffd
landtree <delandtree@gmail.com>
2017-06-07 22:17:38 -0700

    Basic code for running neo pixel on arduino

---
+++ Basic_Arduino_Neo.ino
@@ -0,0 +1,49 @@
+#include <Adafruit_NeoPixel.h>
+
+//defines Neopixels
+#define PIN            8
+#define NUMPIXELS      5
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+
+pixels.begin();
+
+// This initializes the NeoPixel library.
+}
+
+void loop() {
+
+  //Turns on NeoPixels from Pixel 0 to Pixel NUMPIXELS
+  
+for(int i=0;i<NUMPIXELS;i++){
+  pixels.setPixelColor(i, pixels.Color(195,0,182));
+  pixels.show();
+  delay(50);
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
